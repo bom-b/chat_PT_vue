6c54a645f23afb81c28c5dbd3f64570c0d3d5855
heyou <sb97213@gamil.com>
2023-12-23 17:38:10 +0900

    1223

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="app">
-    <Defalut_Header/>
+    <!-- 사용자 정의 헤더 컴포넌트를 동적으로 선택 -->
+    <component :is="headerComponent" />
     <div id="content" class="content">
       <router-view></router-view>
     </div>
   </div>
 </template>
 <script>
-import Defalut_Header from './components/layout/d_header.vue'
+import Default_Header from './components/layout/d_header.vue'
+import PT_Header from './components/layout/pt_header.vue'
 
 export default {
   name: 'App',
   components: {
-    Defalut_Header,
+    Default_Header: Default_Header,
+    PT_Header,
+  },
+  data() {
+    return {
+      // 기본적으로는 Default_Header를 사용
+      headerComponent: Default_Header,
+    };
+  },
+  methods: {
+    // 페이지에 따라 헤더 변경 메서드
+    changeHeader(headerType) {
+      if (headerType === 1) {
+        this.headerComponent = Default_Header;
+      } else if (headerType === 2) {
+        this.headerComponent = PT_Header;
+      }
+    },
   },
 };
 </script>
--- src/router.js
+++ src/router.js
@@ -1,18 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
+import app from './App.vue';
 import Login from "./views/common/login-page.vue";
-import D_Home from "./views/defualt/d_home-page.vue";
-import D_About from "./views/defualt/d_about-page.vue";
+import D_Home from "@/views/default/d_home-page.vue";
+import D_About from "@/views/default/d_about-page.vue";
 import PT_Home from "./views/pt/pt-home-page.vue";
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Login , meta: { headerType: 1 } },
+    { path: "/d_home", component: D_Home, meta: { headerType: 1 } },
+    { path: "/d_about", component: D_About, meta: { headerType: 1 } },
+    { path: "/pt_home", component: PT_Home, meta: { headerType: 2 } },
+  ],
+});
 
-const router = new createRouter({
-    history: createWebHistory(),
-    routes: [
-        {path:"/",component: Login},
-        {path:"/d_home",component: D_Home},
-        {path:"/d_about",component: D_About},
-        {path:"/pt_home",component: PT_Home},
-    ]
-})
+router.beforeEach((to, from, next) => {
+  const headerType = to.meta.headerType || 1;
+  store.commit('setHeaderType', headerType); // 스토어의 뮤테이션 호출
+  next();
+});
 
 export default router;
