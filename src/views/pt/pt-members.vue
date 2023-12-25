5a6ac9ad71c3ca20b91b1f4865d5bbaf407b586a
ysh <yoonsh1028@naver.com>
2023-12-25 22:24:05 +0900

    test

--- src/views/pt/pt-members.vue
+++ src/views/pt/pt-members.vue
@@ -1,16 +1,31 @@
-<template>
-  <div>
-    members
-  </div>
-
-</template>
-
+<!--https://github.com/fullcalendar/fullcalendar-vue?tab=readme-ov-file-->
 <script>
-export default {
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data: function () {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin],
+        initialView: 'dayGridMonth',
+        weekends: false,
+        events: [
+          { title: 'Meeting', start: new Date() }
+        ]
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<template>
+  <h1>스케줄 관리</h1>
+  <calendarOptions>
+    
+  </calendarOptions>
+  <FullCalendar :options='calendarOptions' />  
+</template>
