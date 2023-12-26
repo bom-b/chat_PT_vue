53e69fa72c16f587aacf2761e990dd33a43064cb
chunsik <sb97213@gmail.com>
2023-12-26 18:15:33 +0900

    kakao

--- src/views/default/d-kakao.vue
+++ src/views/default/d-kakao.vue
@@ -2,10 +2,22 @@
   <main id="main">
     <!--  목표달성  -->
     <section>
-      <div class="" style="min-height: 100%; height: auto; padding: 30px;">
+      <div class="green" style="min-height: 100%; height: auto; padding: 30px;">
         <div class="section1800">
+          <div class="row justify-content-center align-items-center" style="margin-top: 50px; text-align: center;">
+            <div class="col-6"
+                 style="display: flex; justify-content: space-between; align-items: flex-start;"
+                 data-aos="fade-in" data-aos-duration="1000" data-aos-delay="200">
 
-
+              <img class="" src="../../assets/img/icon1.png" alt="" style="width: 32px;">
+            </div>
+            <div class="col-6"
+                 style="display: flex; justify-content: space-between; align-items: flex-start;"
+                 data-aos="fade-in" data-aos-duration="1000" data-aos-delay="200">
+              <p class="TheJamsil400" style="display: inline-block;">현재 체중<br>유지</p>
+              <img class="" src="../../assets/img/icon2.png" alt="" style="width: 32px;">
+            </div>
+          </div>
         </div>
       </div>
     </section>
