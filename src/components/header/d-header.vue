81406d0adac9794a5f1a7a17585976b2c0431c43
momorichan <wonkj111@naver.com>
2023-12-27 17:03:34 +0900

    식단 분석 내용 추가 / 변화 추이 추가 / 영양균형 추가 / 식사기록 구현 필요 / 헤더 최적화

--- src/components/header/d-header.vue
+++ src/components/header/d-header.vue
@@ -1,4 +1,6 @@
 <style lang="scss" scoped>
+
+
 nav {
   opacity: 0.9;
 }
@@ -42,46 +44,26 @@ nav {
     margin-top: 50px;
   }
 }
+
+.activeLink {
+  color: #085c57;
+}
 </style>
 
 <template>
-  <nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top"
-       style="min-height: 80px; background-color: white !important;">
+  <nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top" style="min-height: 80px; background-color: white !important;">
     <div class="container-fluid">
       <router-link to="/d_home" class="router-link">
         <img src="../../assets/img/배경지운 로고.png" alt="" style="width: 50px;">
-        <a class="navbar-brand TheJamsil400" style="font-weight: bold;">Chat <span class="TheJamsil400 pine_Green_text"
-                                                                                   style="font-weight: bold;">PT</span></a>
+        <a class="navbar-brand TheJamsil400" style="font-weight: bold;">Chat <span class="TheJamsil400 pine_Green_text" style="font-weight: bold;">PT</span></a>
       </router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="mynavbar">
         <ul class="navbar-nav me-auto">
-          <li class="nav-item">
-            <router-link to="/d_kakao" class="router-link">
-              <a class="nav-link" href="javascript:void(0)" style="color: #085c57;">카카오톡 채널</a>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/d_upload" class="router-link">
-              <a class="nav-link" href="javascript:void(0)">식단등록</a>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/pt_schedule" class="router-link">
-              <a class="nav-link" href="javascript:void(0)">식단 추천 받기</a>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/pt_profile" class="router-link">
-              <a class="nav-link" href="javascript:void(0)">식단 분석</a>
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/pt_profile" class="router-link">
-              <a class="nav-link" href="javascript:void(0)">트레이너 찾기</a>
-            </router-link>
+          <li class="nav-item" v-for="link in navLinks" :key="link.name">
+            <router-link :to="link.route" class="nav-link" :class="{ activeLink: currentRoute === link.route }">{{ link.name }}</router-link>
           </li>
         </ul>
         <div class="profile-img-container d-flex" style="margin-right: 20px;">
@@ -100,5 +82,23 @@ nav {
 <script>
 export default {
   name: "app-header",
+  data() {
+    return {
+      navLinks: [
+        { name: '카카오톡 채널', route: '/d_kakao' },
+        { name: '식단등록', route: '/d_upload' },
+        { name: '식단 추천 받기', route: '/pt_schedule' },
+        { name: '식단 분석', route: '/d_a_change' },
+        { name: '트레이너 찾기', route: '/pt_profile' }
+      ],
+      currentRoute: this.$route.path
+    };
+  },
+  watch: {
+    '$route'(newRoute) {
+      this.currentRoute = newRoute.path;
+    }
+  }
 };
 </script>
+
---
+++ src/views/default/d-a-record.vue
@@ -0,0 +1,22 @@
+<template>
+  <br/>
+  <br/>
+  <br/>
+  <analyze_header/>
+</template>
+
+<style scoped>
+
+</style>
+
+
+
+<script>
+import analyze_header from '@/components/header/d-analyze.vue';
+
+export default {
+  components: {
+    analyze_header,
+  },
+};
+</script>
