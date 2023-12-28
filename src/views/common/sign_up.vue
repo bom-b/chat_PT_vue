39aef4010d9bf7624597a70c9499738792ba32b0
yun <yoonsh1028@naver.com>
2023-12-28 16:53:27 +0900

    회원가입

--- src/views/common/sign_up.vue
+++ src/views/common/sign_up.vue
@@ -1,48 +1,105 @@
 <template>
 	<main>
-		<form v-on:submit.prevent="submitForm">
-			<div>
-				<label for="username">id: </label>
-				<input id="username" type="text" v-model="username">
+		<div>
+			<h2>회원가입</h2>
+
+			<div class="btn-group">
+				<button v-for="categoryOption in categoryOptions" :key="categoryOption.value"
+					:class="['btn', 'btn-secondary', { 'active': category === categoryOption.value }]"
+					@click="selectCategory(categoryOption.value)">
+					{{ categoryOption.label }}
+				</button>
 			</div>
+
 			<div>
-				<label for="password">pw: </label>
-				<input id="password" type="password" v-model="password">
+				<label>이메일:</label>
+				<input type="email" v-model="email" class="form-control">
 			</div>
-			<button type="submit">login</button>
-		</form>
+
+			<div>
+				<label>이름:</label>
+				<input type="text" v-model="name" class="form-control">
+			</div>
+
+			<div>
+				<label>나이:</label>
+				<input type="number" v-model="age" class="form-control">
+			</div>
+
+			<div>
+				<label>키:</label>
+				<input type="number" v-model="height" class="form-control">
+			</div>
+
+			<div>
+				<label>체중:</label>
+				<input type="number" v-model="weight" class="form-control">
+			</div>
+
+			<div>
+				<label>성별:</label>
+				<div>
+					<div class="form-check form-check-inline">
+						<input class="form-check-input" type="radio" id="male" value="남성" v-model="gender">
+						<label class="form-check-label" for="male">남성</label>
+					</div>
+					<div class="form-check form-check-inline">
+						<input class="form-check-input" type="radio" id="female" value="여성" v-model="gender">
+						<label class="form-check-label" for="female">여성</label>
+					</div>
+				</div>
+			</div>
+
+			<button @click="submitForm" class="btn btn-primary">가입하기</button>
+		</div>
 	</main>
 </template>
   
 <script>
-import axios from 'axios';
-
 export default {
-	data: function () {
+	data() {
 		return {
-			username: '',
-			password: '',
-		}
+			category: '일반회원',
+			categoryOptions: [
+				{ label: '일반회원', value: '일반회원' },
+				{ label: 'PT회원', value: 'PT회원' }
+			],
+			email: '',
+			name: '',
+			age: null,
+			height: null,
+			weight: null,
+			gender: ''
+		};
 	},
 	methods: {
-		submitForm: function () {
-			// event.preventDefault();
-			console.log(this.username, this.password);
-			const url = 'https://jsonplaceholder.typicode.com/users';
-			const data = {
-				username: this.username,
-				password: this.password
-			}
-			axios.post(url, data)
-				.then(function (response) {
-					console.log(response);
-				})
-				.catch(function (error) {
-					console.log(error);
-				});
+		selectCategory(category) {
+			this.category = category;
+		},
+		submitForm() {
+			// 회원가입 폼 제출 로직 구현
+			console.log('회원가입 폼 제출:', this.category, this.email, this.name, this.age, this.height, this.weight, this.gender);
 		}
 	}
-}
+};
 </script>
   
-<style></style>
+<style scoped>
+.btn-group {
+	display: flex;
+	margin-bottom: 1rem;
+}
+
+.btn {
+	flex: 1;
+}
+
+.form-control {
+	width: 100%;
+	margin-bottom: 1rem;
+}
+
+.form-check-inline {
+	margin-right: 1rem;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   transpileDependencies: [
-    // 여기에 transpile이 필요한 의존성 패키지들을 추가
-    // 예: 'axios', 'vue-awesome'
+    'axios',
   ]
 };
