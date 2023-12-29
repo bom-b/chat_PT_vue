485bdb7db30e43a137bc8800fb0c43b2cda31234
minssup <bms991117@naver.com>
2023-12-29 14:52:32 +0900

    Update d-diet-recommand.vue

--- src/views/default/d-diet-recommand.vue
+++ src/views/default/d-diet-recommand.vue
@@ -1,33 +1,30 @@
 <template>
   <div class="scroll-container">
     <!--  섹션1  -->
-    <div class="scroll-area green"
+    <div class="scroll-area"
          style="display: flex; justify-content: center; align-items: center; min-height: 100vh;">
-      <div class="row section1400">
-        <div id="phone-col" class="col-lg-6 col-sm-12" data-aos="fade-in" data-aos-duration="1000" data-aos-delay="100">
-          <img id="phone" class="" src="../../assets/img/kakaoplus.png" alt=""
-               style="width: 400px; object-fit: contain;">
-        </div>
-        <div class="col-lg-6 col-sm-12" data-aos="fade-in" data-aos-duration="1000" data-aos-delay="300">
-          <div class="description">
-            <div id="chun-icon">
-              <img class="" src="../../assets/img/헤헷.png" alt="" style="">
-            </div>
-            <p class="TheJamsil400 font25" data-aos="fade-in" data-aos-duration="2000" data-aos-delay=""
-               style=" color: #FFFFFF;"><span class="TheJamsil400 font25"
-                                              style="color: #FEE500; margin-top: 20px;">카카오톡</span>채널
-              추가하고</p>
-            <p class="TheJamsil400 font15" data-aos="fade-in" data-aos-duration="2000" data-aos-delay=""
-               style="color: #00997b; margin-top: 20px; ">어디서나 간편하게 사용해보세요.</p>
-            <div id="kakao-plus-btn">
-              <a href="https://pf.kakao.com/_xccVXG">
-                <!--                    <img class="" src="../../assets/img/QR.png" alt="" style="width: 100px;">-->
-                <img class="" src="../../assets/img/friendadd_large_yellow_rect.png" alt="" style="">
-              </a>
-            </div>
-          </div>
-        </div>
+      <div class="row" style="margin-top: 50px; text-align: center; display: flex;">
+
+      <div style="margin-bottom: 100px;"
+          data-aos="fade-in" data-aos-duration="1000" data-aos-delay="100">
+        <span class="material-icons pine_Green_text">bar_chart</span>
+        <span id="" class="pine_Green_text TheJamsil400"
+              style="white-space: nowrap; font-size: 1.7rem;">나만의 맞춤 음식 추천</span>
       </div>
+
+      <div class="col-lg-6 col-sm-6" style="height: 600px; text-align: center;"
+          data-aos="fade-in" data-aos-duration="1000" data-aos-delay="200">
+        <p class="TheJamsil400" style="white-space: nowrap; font-size: 1.3rem; margin-bottom: 30px;">오늘의 칼로리</p>
+        <img src="../../assets/img/메인_칼로리.png" style="max-height: 40%; object-fit: contain; " alt="s">
+      </div>
+
+      <div class="col-lg-6 col-sm-6" style="height: 600px; text-align: center;"
+          data-aos="fade-in" data-aos-duration="1000" data-aos-delay="300">
+        <p class="TheJamsil400" style="white-space: nowrap; font-size: 1.3rem; margin-bottom: 30px;">오늘의 영양소</p>
+        <img src="../../assets/img/바차트.png" style="max-height: 40%; object-fit: contain; " alt="s">  
+      </div>
+      
+      </div><!-- 사진 등록 -->
     </div>
     <!--  섹션2  -->
     <div class="scroll-area ivory" style="display: flex; justify-content: center; align-items: center;">
