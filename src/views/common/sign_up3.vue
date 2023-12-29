83fd1c11dbb5e08a15b683d2bcbbfbd94b7a1212
yun <yoonsh1028@naver.com>
2023-12-29 20:14:46 +0900

    성별추가

--- src/views/common/sign_up3.vue
+++ src/views/common/sign_up3.vue
@@ -9,12 +9,23 @@
       <div class="input-container">
         <ul>
           <li>
+            <label for="sex">성별</label>
+            <div class="btn-group" role="group">
+              <button type="button" class="btn" :class="{ 'btn-male': sex === 'male' }" @click="setSex('male')">
+                <i class="material-icons">male</i>
+              </button>
+              <button type="button" class="btn" :class="{ 'btn-female': sex === 'female' }" @click="setSex('female')">
+                <i class="material-icons">female</i>
+              </button>
+            </div>
+          </li>
+          <li>
             <label for="height">키(cm)</label>
             <input class="form-control" type="number" id="height" v-model="height">
           </li>
           <li>
             <label for="weight">몸무게(kg)</label>
-            <input class='form-control' type="number" id="weight" v-model="weight">
+            <input class="form-control" type="number" id="weight" v-model="weight">
           </li>
           <li>
             <label for="bmi"> bmi </label>
@@ -24,7 +35,7 @@
       </div>
 
       <div class="button-container">
-        <button type="button" class="btn btn-primary" @click="proceedToNextPage" :disabled="!height || !weight">
+        <button type="button" class="btn btn-primary" @click="proceedToNextPage" :disabled="!height || !weight || !sex">
           다음
         </button>
       </div>
@@ -38,6 +49,26 @@
   justify-content: center;
   margin-top: 20px;
 }
+
+.btn-group {
+  display: flex;
+}
+
+.btn-group button {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+}
+
+.btn-male {
+  background-color: skyblue;
+}
+
+.btn-female {
+  background-color: pink;
+}
 </style>
 
 <script>
@@ -47,11 +78,12 @@ export default {
       height: '',
       weight: '',
       BMI: '',
+      sex: '',
     };
   },
   computed: {
     progress() {
-      if (this.height && this.weight) {
+      if (this.height && this.weight && this.sex) {
         return 75;
       } else {
         return 50;
@@ -60,24 +92,22 @@ export default {
   },
   methods: {
     proceedToNextPage() {
-      if (this.height && this.weight) {
-        if (this.hasAllergy === 'yes') {
-          this.showAllergyToast = true;
-        } else {
-          this.$router.push('/next-page');
-        }
+      if (this.height && this.weight && this.sex) {
+        // 다음 페이지로 이동
       } else {
-        // 키 또는 몸무게가 입력되지 않았을 때 처리
-        alert('키와 몸무게를 입력해주세요!');
+        alert('키, 몸무게, 성별을 입력해주세요!');
       }
     },
     calculateBMI() {
       if (this.height && this.weight) {
-        this.BMI = (this.weight / ((this.height / 100) ** 2)).toFixed(2); // 키를 cm에서 m로 변환하고 소수점 두 자리까지 계산
+        this.BMI = (this.weight / ((this.height / 100) ** 2)).toFixed(2);
         return this.BMI;
       } else {
         return '';
       }
+    },
+    setSex(sex) {
+      this.sex = sex;
     },
   },
 };
