7cf666ca241124296922ac2a0b35a41341083e2d
nias2734 <nias2734@naver.com>
2023-12-29 18:48:37 +0900

    투명도 해결해줘

---
+++ src/components/header/mypage/WidgetContent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-if="widgetVisible" class="floating-widget">
+      <h2>My page</h2>
+      <p>○○○ 님의 데이터입니다.</p>
+      <div></div>
+    </div>
+  </template>
+  
+<script>
+export default {
+  props: {
+    widgetVisible: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+
+/* 필요한 스타일 */
+.floating-widget {
+  width: 900px;
+  opacity: 1 !important;
+}
+</style>
