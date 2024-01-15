7d548333f3450cdaa2aa239f11b4c0054234e1f8
yun <yoonsh1028@naver.com>
2024-01-15 20:03:53 +0900

    333

--- src/views/signup/d_signup/sign_up.vue
+++ src/views/signup/d_signup/sign_up.vue
@@ -1,10 +1,14 @@
 <script>
+import Swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 export default {
 	data() {
 		return {
 			user: {
+				id: '',
 				name: '',
 				email: '',
+				password: '',
 			},
 			// 이메일 중복체크
 			checkEmail: null,
@@ -12,30 +16,21 @@ export default {
 			password_Check: '',
 			isPasswordMismatch: false,
 			inputemail: 'white',
+			inputid: 'white',
 			whyNotInsert: false,
-			isSend: false,
-
-
+			sendmessage: '',
+			serverCode:'',
 		};
 	},
 	computed: {
 		progress() {
-			if (this.user.name && this.user.email && this.authCode && this.password && this.password === this.password_Check) {
-				return 25;
-			} else {
-				return 0;
-			}
+			return this.user.name && this.user.email && this.authCode && this.user.password && this.user.password === this.password_Check ? 25 : 0;
 		},
 		isFormValid() {
-			return (
-				this.name &&
-				this.email &&
-				this.authCode &&
-				this.password &&
-				this.password === this.password_Check
-			);
+			return this.user.name && this.user.email && this.authCode && this.user.password && this.user.password === this.password_Check;
 		},
 	},
+
 	watch: {
 		password(newPassword) {
 			if (!newPassword && !this.password_Check) {
@@ -45,14 +40,27 @@ export default {
 			}
 		},
 		password_Check(newPasswordCheck) {
-			if (!this.password && !newPasswordCheck) {
+			if (!this.user.password && !newPasswordCheck) {
 				this.matchpwd = false;
 			} else {
-				this.matchpwd = this.password !== newPasswordCheck;
+				this.matchpwd = this.user.password !== newPasswordCheck;
 			}
 		},
 	},
 	methods: {
+		// 아이디 정규표현식
+		checkId(id) {
+			if (id !== undefined) {
+				const reg = /^[a-zA-Z0-9]+$/;
+				if (id.match(reg)) {
+					return id;
+				}
+			}
+			return ("되겠냐");
+		},
+		idChange() {
+			this.inputid = 'white';
+		},
 		emailChange() {
 			this.inputemail = 'white';
 		},
@@ -60,48 +68,95 @@ export default {
 		removeSpaces(email) {
 			return email.trim();
 		},
+		// id 중복체크
+		async IdCheck() {
+			try {
+				const regId = this.checkId(this.user.id)
+				const response = await this.$axiosWithoutValidation.post(`/signUp/id/${regId}`)
+				const checkedId = parseInt(response.data);
+				if (checkedId < 1) {
+					const result = await Swal.fire({
+						title: '',
+						text: '사용 가능한 아이디입니다. 사용하시겠습니까?',
+						icon: 'warning',
+						showCancelButton: true,
+						confirmButtonColor: '#3085d6',
+						cancelButtonColor: '#d33',
+						confirmButtonText: '확인',
+						cancelButtonText: '취소',
+					});
+
+					if (result.isConfirmed) {
+						this.inputid = 'gray';
+					}
+				}
+			} catch (e) {
+				console.log(e)
+			}
+		},
+		confirm() {
+			this.finalResults = ''
+			if (!this.isSend) {
+				this.finalResult = '인증번호 요청을 먼저 해주세요.'
+			}
+			else if (this.authCode === '') {
+				this.finalResult = '인증번호를 입력해주세요.'
+			} else if (this.authCode == this.serverCode) {
+				this.isCorrect = true;
+			} else {
+				this.finalResult = '인증번호가 일치하지 않습니다.'
+			}
+		},
 
 		// email 중복체크
-		async duplicateCheck() {
+		async EmailCheck() {
 			try {
 				const clearemail = this.removeSpaces(this.user.email);
-				const response = await this.$axiosWithoutValidation.post(`/validCheckEmail/${clearemail}`);
+				const response = await this.$axiosWithoutValidation.post(`/signUp/email/${clearemail}`);
 				console.log('email체크', response);
+
 				// 문자열로 오는 경우 숫자로 변환
 				const emailCount = parseInt(response.data);
 
 				if (emailCount < 1) {
-					if (confirm("사용 가능한 이메일 입니다. 계속 진행하시겠습니까?")) {
+					const result = await Swal.fire({
+						title: '사용 가능한 이메일 입니다.',
+						text: '계속 진행하시겠습니까?',
+						icon: 'question',
+						showCancelButton: true,
+						confirmButtonColor: '#3085d6',
+						cancelButtonColor: '#d33',
+						confirmButtonText: '확인',
+						cancelButtonText: '취소',
+					});
+
+					if (result.isConfirmed) {
 						this.inputemail = 'gray';
 						this.sendMail();
 					}
 				} else {
-					alert("이미 사용중인 이메일 입니다.");
+					await this.$swal("이미 사용중인 이메일 입니다.");
 				}
 			} catch (e) {
 				console.log('email 중복 체크 에러', e);
 			}
 		},
 
-
 		sendMail() {
-			this.goodResult = '확인 중입니다. 잠시만 기다려주세요.'
-			this.isSend = true;
 			this.$axiosWithoutValidation.post(`/service/authemail/${this.user.email}`)
 				.then((response) => {
-					this.goodResult = '인증번호가 발송 되었습니다.<br>이메일을 확인해주세요.';
+					this.sendmessage = '인증번호가 발송 되었습니다.<br>이메일을 확인해주세요.';
 					this.serverCode = response.data;
-					
 				}).catch(() => {
 					this.goodResult = '';
 					this.badResult = '입력하신 정보와 일치하는 회원이 없습니다.<br>해당 정보를 다시 확인하거나 회원가입을 진행해주세요.'
 				})
-		}
-	},
-	proceedToNextPage() {
-		if (this.user.name && this.user.email && this.authCode && this.password && this.password === this.password_Check) {
-			// 필요한 모든 정보가 입력되었고, 비밀번호와 비밀번호 확인이 일치할 때 다음 페이지로 이동
-			this.$router.push('/signUp/sign_up2');
+		},
+		proceedToNextPage() {
+			const isValid = this.user.name && this.user.email && this.authCode && this.user.password && this.user.password === this.password_Check;
+			if (isValid) {
+				this.$emit('page1-data', this.user);
+			}
 		}
 	},
 };
@@ -115,34 +170,46 @@ export default {
 		<div class="container">
 			<div class="login-container">
 				<h2 class="mb-4">회원가입</h2>
-
+				<!-- 아이디 입력 폼 -->
+				<div class="row mb-3">
+					<label for="id" class="col-sm-2 col-form-label">아이디:</label>
+					<div class="col-sm-8">
+						<div class="input-group">
+							<input type="text" class="form-control" id="id" placeholder="아이디를 입력하세요" v-model="user.id"
+								:style="{ 'background-color': inputid }" @input="idChange">
+							<button class="w-btn w-btn-gra-anim w-btn-gra3" type="button" @click="IdCheck">아이디 중복확인</button>
+						</div>
+					</div>
+				</div>
 				<!-- 이름 입력 폼 -->
 				<div class="form-group">
 					<label for="name">이름:</label>
 					<input type="text" class="form-control" id="name" placeholder="이름을 입력하세요" v-model="user.name">
 				</div>
-				<p v-html="whyNotInsert"></p>
 				<!-- 이메일 입력 폼 -->
-				<form @submit.prevent="sendMail" class="mt-5">
+				<form @submit.prevent="EmailCheck" class="mt-5">
 					<div class="form-group">
 						<label for="email">이메일:</label>
-						<input type="email" class="form-control" id="email" placeholder="이메일을 입력하세요" v-model="user.email"
-							:style="{ 'background-color': inputemail }" @input="emailChange">
-						<button type="submit" class="btn btn-primary" @click="duplicateCheck">이메일 확인</button>
+						<div class="input-group">
+							<input type="email" class="form-control" id="email" placeholder="이메일을 입력하세요"
+								v-model="user.email" :style="{ 'background-color': inputemail }" @input="emailChange">
+							<button type="submit" class="btn btn-primary">이메일 확인</button>
+						</div>
 					</div>
 				</form>
 				<!-- 인증번호 입력 폼 -->
 				<div class="form-group">
 					<label for="authCode">인증번호:</label>
-					<input type="text" class="form-control" id="authCode" placeholder="인증번호를 입력하세요" v-model="authCode">
-				</div>
-				<div>
-
+					<div class="input-group">
+						<input type="text" class="form-control" id="authCode" placeholder="인증번호를 입력하세요" v-model="authCode">
+						<button type="submit" class="btn btn-primary">인증번호 확인</button>
+					</div>
 				</div>
 				<!-- 비밀번호 입력 폼 -->
 				<div class="form-group">
 					<label for="password">비밀번호:</label>
-					<input type="password" class="form-control" id="password" placeholder="비밀번호를 입력하세요" v-model="password">
+					<input type="password" class="form-control" id="password" placeholder="비밀번호를 입력하세요"
+						v-model="user.password">
 				</div>
 				<div class="form-group">
 					<label for="password_Check">비밀번호 확인:</label>
@@ -154,7 +221,7 @@ export default {
 				</div>
 				<div class="button-container">
 					<button type="button" class="btn btn-primary" @click="proceedToNextPage"
-						:disabled="!name || !email || !authCode || !password || !password_Check && (password !== password_Check)">다음</button>
+						:disabled="!user.id || !user.name || !user.email || !authCode || !user.password || !password_Check && (user.password !== password_Check)">다음</button>
 				</div>
 			</div>
 		</div>
---
+++ src/views/signup/d_signup/sign_up_master.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div v-for="(page, index) in pages" :key="index">
+            <!-- 각 페이지 컴포넌트에 이벤트 수신기 등록 -->
+            <component :is="page" @page1-data="handlePage1Data"></component>
+        </div>
+    </div>
+</template>
+  
+<script>
+import signUp from "@/views/signup/d_signup/sign_up.vue";
+import signUp2 from "@/views/signup/d_signup/sign_up2.vue";
+import signUp3 from "@/views/signup/d_signup/sign_up3.vue";
+import signUp4 from "@/views/signup/d_signup/sign_up4.vue";
+
+export default {
+    methods: {
+        handlePage1Data(userData) {
+            // userData를 사용하여 부모 컴포넌트에서 원하는 작업 수행
+            console.log('받은 데이터:', userData);
+        },
+    },
+    data() {
+        return {
+            pages: [signUp, signUp2, signUp3, signUp4],
+        };
+    },
+};
+</script>
+  
