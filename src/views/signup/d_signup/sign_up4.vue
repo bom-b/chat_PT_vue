78c30b787ac0399f012e00ea97dbae93dc639cb5
yun <yoonsh1028@naver.com>
2024-01-19 14:32:44 +0900

    11

--- src/views/signup/d_signup/sign_up4.vue
+++ src/views/signup/d_signup/sign_up4.vue
@@ -8,14 +8,28 @@ export default {
         };
     },
     computed: {
-        // progress() {
-        //     return () ? 100 : 75; // 개인정보 동의가 완료되면 50%의 진행 상태를 나타냅니다.
-        // },
+        progress() {
+            return (this.rating.length) ? 100 : 75; // 개인정보 동의가 완료되면 50%의 진행 상태를 나타냅니다.
+        },
     },
     created() {
         this.getfoodList(); // getfoodList 메소드를 호출하여 음식 리스트를 가져옵니다.
     },
     methods: {
+        selectFood(food) {
+            if (this.rating.includes(food.foodnum)) {
+                // 이미 선택된 이미지인 경우 선택을 해제합니다.
+                this.rating = this.rating.filter(num => num !== food.foodnum);
+            } else {
+                // 선택되지 않은 이미지인 경우 선택을 추가합니다.
+                if (this.rating.length < 5) {
+                    this.rating.push(food.foodnum);
+                } else {
+                    // 이미 5개의 이미지를 선택한 경우 추가적인 선택은 불가능합니다.
+                    this.$swal("5개의 음식 이미지만 선택할 수 있습니다.");
+                }
+            }
+        },
         proceedToNextPage() {
             try {
                 const isValid = 1;
@@ -61,7 +75,7 @@ export default {
             <p>음식 추천을 할때 도와드려요</p>
             <div class="input-container">
                 <ul>
-                    <li v-for="food in foodList" :key="food.foodnum">
+                    <li v-for="food in foodList" :key="food.foodnum" :class="{ selected: rating.includes(food.foodnum) }">
                         <img :src="getImagePath(food.FOODIMG)" alt="음식 이미지">
                         <span>{{ food.FOODNAME }}</span>
                     </li>
@@ -71,28 +85,40 @@ export default {
     </main>
 </template>
 
+
 <style scoped>
-.container {
-    margin-top: 95px;
-}
-
-.button-container {
-    display: flex;
-    justify-content: center;
-    margin-top: 20px;
-}
-
-.radio-group {
-    display: flex;
-    flex-direction: column;
-}
-
-.radio-group label {
-    margin-bottom: 5px;
-}
-
 .selected {
     border: 7px solid green;
+}
 
+li {
+    list-style-type: none;
+    position: relative;
+    padding-left: 30px;
+}
+
+li img {
+    width: 250px;
+    height: 250px;
+    object-fit: contain;
+    object-position: center;
+}
+
+li::before {
+    content: "";
+    display: inline-block;
+    position: absolute;
+    left: 0;
+    width: 20px;
+    /* 원하는 너비로 설정 */
+    height: 20px;
+    /* 원하는 높이로 설정 */
+    background-image: url('../../../assets/img/gif/loading.gif');
+    background-size: cover;
+    /* 이미지가 요소를 완전히 덮도록 설정, 필요에 따라 'contain'으로 변경 가능 */
+    background-repeat: no-repeat;
+    /* 이미지가 반복되지 않도록 설정 */
+    background-position: center;
+    /* 이미지가 요소의 중앙에 위치하도록 설정 */
 }
 </style>
