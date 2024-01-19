f72b89013c743677c28a5fd7f48f1b52624d2b59
nias2734 <nias2734@naver.com>
2024-01-19 17:01:33 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,25 @@ module.exports = {
     'axios',
   ],
   devServer: {
-    allowedHosts: 'all'
+    allowedHosts: 'all',
+    ws: false // 여기에 ws 설정을 false로 추가했습니다.
   },
 };
+
+// const webpack = require("webpack");
+// module.exports = {
+//   configureWebpack: {
+//     plugins: [
+//       new webpack.DefinePlugin({
+//         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+//       }),
+//     ],
+//   },
+//   transpileDependencies: [
+//     'axios',
+//   ],
+//   devServer: {
+//     allowedHosts: 'all',
+//     ws: false // 여기에 ws 설정을 false로 추가했습니다.
+//   },
+// };
